d9f99179e6faec3fc1c975de244ad68efa67a14d
Allusive <154700875+AllusiveWheat@users.noreply.github.com>
2025-06-30 13:19:06 -0700

    and the description

--- src/index.ts
+++ src/index.ts
@@ -222,6 +222,9 @@ client.on("interactionCreate", async (interaction) => {
     // filter out color codes from server names
     sorted.forEach((guild: any) => {
       guild.host_name = guild.host_name.replace(/\^[A-Fa-f0-9]{6,8}/g, "");
+      guild.description = guild.description
+        ? guild.description.replace(/\^[A-Fa-f0-9]{6,8}/g, "")
+        : "";
     });
 
     pagination.setFields(
